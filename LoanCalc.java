6267a39ed65b1e846f12de8e4db200e4e7deefdd
Dan Dover <45949873+doverdan@users.noreply.github.com>
2024-02-15 14:40:40 +0200

    submit

--- LoanCalc.java
+++ LoanCalc.java
@@ -1,66 +1,71 @@
-/**
-* Computes the periodical payment necessary to re-pay a given loan.
-*/
-public class LoanCalc {
-	
-	static double epsilon = 0.001;  // The computation tolerance (estimation error)
-	static int iterationCounter;    // Monitors the efficiency of the calculation
-	
-    /** 
-     * Gets the loan data and computes the periodical payment.
-     * Expects to get three command-line arguments: sum of the loan (double),
-     * interest rate (double, as a percentage), and number of payments (int).  
-     */
-	public static void main(String[] args) {		
-		// Gets the loan data
-		double loan = Double.parseDouble(args[0]);
-		double rate = Double.parseDouble(args[1]);
-		int n = Integer.parseInt(args[2]);
-		System.out.println("Loan sum = " + loan + ", interest rate = " + rate + "%, periods = " + n);
-		
-		// Computes the periodical payment using brute force search
-		System.out.print("Periodical payment, using brute force: ");
-		System.out.printf("%.2f", bruteForceSolver(loan, rate, n, epsilon));
-		System.out.println();
-		System.out.println("number of iterations: " + iterationCounter);
 
-		// Computes the periodical payment using bisection search
-		System.out.print("Periodical payment, using bi-section search: ");
-		System.out.printf("%.2f", bisectionSolver(loan, rate, n, epsilon));
-		System.out.println();
-		System.out.println("number of iterations: " + iterationCounter);
+public class Calendar {
+	// Starting the calendar on 1/1/1900
+	static int dayOfMonth = 1;
+	static int month = 1;
+	static int year = 1900;
+	static int dayOfWeek = 2;
+	static int nDaysInMonth = 31;
+
+	public static void main(String args[]) {
+
+		int ChosenYear = Integer.parseInt(args[0]);
+		while (year <= ChosenYear) {
+			advance();
+			if (year == ChosenYear) {
+				if (dayOfWeek == 1) {
+					System.out.println(dayOfMonth + "/" + month + "/" + year + " Sunday");
+				} else {
+					System.out.println(dayOfMonth + "/" + month + "/" + year);
+				}
+
+			}
+		}
+
 	}
-	
-	/**
-	* Uses a sequential search method  ("brute force") to compute an approximation
-	* of the periodical payment that will bring the ending balance of a loan close to 0.
-	* Given: the sum of the loan, the periodical interest rate (as a percentage),
-	* the number of periods (n), and epsilon, a tolerance level.
-	*/
-	// Side effect: modifies the class variable iterationCounter.
-    public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {  
-    	// Replace the following statement with your code
-    	return 0;
-    }
-    
-    /**
-	* Uses bisection search to compute an approximation of the periodical payment 
-	* that will bring the ending balance of a loan close to 0.
-	* Given: the sum of theloan, the periodical interest rate (as a percentage),
-	* the number of periods (n), and epsilon, a tolerance level.
-	*/
-	// Side effect: modifies the class variable iterationCounter.
-    public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-    	// Replace the following statement with your code
-    	return 0;
-    }
-	
-	/**
-	* Computes the ending balance of a loan, given the sum of the loan, the periodical
-	* interest rate (as a percentage), the number of periods (n), and the periodical payment.
-	*/
-	private static double endBalance(double loan, double rate, int n, double payment) {
-		// Replace the following statement with your code
-    	return 0;
+
+	private static void advance() {
+		dayOfMonth++;
+		dayOfWeek++;
+		if (dayOfWeek > 7)
+			dayOfWeek = 1;
+		nDaysInMonth = nDaysInMonth(month, year);
+		if (dayOfMonth > nDaysInMonth) {
+			dayOfMonth = 1;
+			month++;
+			if (month > 12) {
+				month = 1;
+				year++;
+			}
+		}
 	}
+
+	private static boolean isLeapYear(int year) {
+		if (year % 4 == 0) {
+			if (year % 100 == 0) {
+				if (year % 400 == 0) {
+					return true;
+				}
+				return false;
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private static int nDaysInMonth(int month, int year) {
+		if (month == 2) {
+			if (isLeapYear(year)) {
+				return 29;
+			}
+			return 28;
+		}
+		if (month == 4 || month == 6 || month == 9 || month == 11) {
+			return 30;
+		} else {
+			return 31;
+		}
+	}
+
 }
